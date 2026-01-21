4a9b97b1fad2830320591cf7fb498a2920daf510
Gabriel Novaes <semproblema@gmail.com>
2026-01-21 16:09:53 -0400

    fix

--- core/deploy_to_prod_v2.sh
+++ core/deploy_to_prod_v2.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+# Diferencas em relacao ao v1
+# - Aceita trocar de branch, recebendo no parametro 4 o nome da branch atual
+
 # ---------------------------------------------
 # This script will be run after deploy in prod (Without staging) has been run
 # ---------------------------------------------
@@ -14,12 +17,21 @@ THEME_DIR=$3
 CURRENT_BRANCH=$4
 
 # ============================
+# INSTALAÇÃO DO TEMA
+# ============================
+rm -rf /home/$USER/$DOMAIN/resources/$THEME_DIR
+mkdir -p /home/$USER/$DOMAIN/resources
+cp -R /home/themes/$THEME_DIR/. /home/$USER/$DOMAIN/resources/$THEME_DIR/
+
+
+# ============================
 # DEPLOY DA APLICAÇÃO
 # ============================
 cd /home/$USER/$DOMAIN
 
 git pull origin $CURRENT_BRANCH
 composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+
 
 # ============================
 # MIGRAÇÕES E OTIMIZAÇÕES
@@ -33,18 +45,12 @@ php artisan about
 # Gera arquivo com informações do último commit
 git log -1 --pretty="Autor: %an%nData: %ad%nMensagem:%n%B" > public/dnversion.txt
 
-# ============================
-# INSTALAÇÃO DO TEMA
-# ============================
-rm -rf /home/$USER/$DOMAIN/resources/$THEME_DIR
-mkdir -p /home/$USER/$DOMAIN/resources
-cp -R /home/themes/$THEME_DIR/. /home/$USER/$DOMAIN/resources/$THEME_DIR/
 
+# ============================
+# CONFIGURAÇÃO DO NPM (USER-ONLY) e INSTALAÇÃO DE DEPENDÊNCIAS
+# ============================
 cd /home/$USER/$DOMAIN/resources/$THEME_DIR/
 
-# ============================
-# CONFIGURAÇÃO DO NPM (USER-ONLY)
-# ============================
 npm config set cache ~/.npm
 mkdir -p ~/.npm-global/bin
 npm config set prefix ~/.npm-global
