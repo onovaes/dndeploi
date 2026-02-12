6aac2cfb559e718aad41126399cb0016b64852c2
Gabriel Novaes <semproblema@gmail.com>
2026-02-11 20:06:58 -0400

    fix: update PHP-FPM service command from restart to reload in deployment script

--- admin/deploy_to_prod_v2.sh
+++ admin/deploy_to_prod_v2.sh
@@ -51,9 +51,9 @@ git log -1 --pretty="Branch: $CURRENT_BRANCH%nAutor: %an%nData: %ad%nMensagem:%n
 
 
 # ============================
-# RESTART PHP-FPM
+# RELOAD PHP-FPM
 # ============================
-echo "ℹ️ Restarting PHP-FPM service..."
-echo "" | sudo -S service php8.4-fpm restart
+echo "ℹ️ Reloading PHP-FPM service..."
+echo "" | sudo -S service php8.4-fpm reload
 
 echo "🚀 SUCCESS! Admin Application deployed!"
